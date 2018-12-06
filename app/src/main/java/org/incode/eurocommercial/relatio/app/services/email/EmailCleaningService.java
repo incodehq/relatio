e654f79ff9c478bc251d4d904c61f99fb9cc682f
Jeroen van der Wal <jeroen@stromboli.it>
2018-12-06 09:52:51 +0100

    REL-22 Add email cleanin service

---
+++ app/src/main/java/org/incode/eurocommercial/relatio/app/services/email/EmailCleaningService.java
@@ -0,0 +1,14 @@
+package org.incode.eurocommercial.relatio.app.services.email;
+
+import org.apache.isis.applib.annotation.DomainService;
+import org.apache.isis.applib.annotation.NatureOfService;
+
+@DomainService(nature = NatureOfService.DOMAIN)
+public class EmailCleaningService {
+
+    public String clean(final String input){
+
+        return null;
+    }
+
+}
