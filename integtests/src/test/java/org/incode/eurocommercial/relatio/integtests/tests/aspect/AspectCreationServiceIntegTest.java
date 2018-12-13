fe2f90b24d26313dedb6154477d2ef846db7f326
Maré du Preez <maredpz@hotmail.nl>
2018-12-13 17:46:07 +0100

    REL-24: added unit and integ tests

---
+++ integtests/src/test/java/org/incode/eurocommercial/relatio/integtests/tests/aspect/AspectCreationServiceIntegTest.java
@@ -0,0 +1,60 @@
+package org.incode.eurocommercial.relatio.integtests.tests.aspect;
+
+import org.apache.isis.applib.fixturescripts.FixtureScripts;
+import org.incode.eurocommercial.relatio.dom.aspect.Aspect;
+import org.incode.eurocommercial.relatio.dom.aspect.AspectCreationService;
+import org.incode.eurocommercial.relatio.dom.aspect.AspectRepository;
+import org.incode.eurocommercial.relatio.dom.event.Event;
+import org.incode.eurocommercial.relatio.dom.event.EventRepository;
+import org.incode.eurocommercial.relatio.dom.profile.ProfileRepository;
+import org.incode.eurocommercial.relatio.fixture.dom.event.WifiprojectAccessiEventFixture;
+import org.incode.eurocommercial.relatio.integtests.tests.RelatioIntegTest;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.inject.Inject;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AspectCreationServiceIntegTest extends RelatioIntegTest {
+
+    @Inject
+    private FixtureScripts fixtureScripts;
+
+    @Inject
+    EventRepository eventRepository;
+
+    @Inject
+    AspectRepository aspectRepository;
+
+    @Inject
+    AspectCreationService aspectCreationService;
+
+    @Inject
+    ProfileRepository profileRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        // given
+        fixtureScripts.runFixtureScript(new WifiprojectAccessiEventFixture(), null);
+    }
+
+    public static class AspectCreationService_Test extends AspectCreationServiceIntegTest{
+        @Test
+        public void createAspectsFromEvent_happy_case(){
+            //given
+            Event event = eventRepository.listAll().get(0);
+            int sizeBeforeAction = profileRepository.listAll().size();
+
+            //when
+            aspectCreationService.createAspectsFromEvent(event);
+
+            //then
+            List<Aspect> aspectsList = aspectRepository.findByEvent(event);
+            assertThat(aspectsList).isNotEmpty();
+            assertThat(profileRepository.listAll().size()).isGreaterThan(sizeBeforeAction);
+
+        }
+    }
+}
