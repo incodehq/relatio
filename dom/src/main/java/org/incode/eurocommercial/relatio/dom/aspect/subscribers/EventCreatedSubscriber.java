59cea36c52bdd07f942534fbc5ad728095958c5d
Marcus van Bergen <mbhjvanbergen@gmail.com>
2018-12-14 14:41:54 +0100

    REL-24: fixed event handing using axon

--- dom/src/main/java/org/incode/eurocommercial/relatio/dom/aspect/subscribers/EventCreatedSubscriber.java
+++ dom/src/main/java/org/incode/eurocommercial/relatio/dom/aspect/subscribers/EventCreatedSubscriber.java
@@ -1,9 +1,9 @@
 package org.incode.eurocommercial.relatio.dom.aspect.subscribers;
 
-import com.google.common.eventbus.Subscribe;
 import org.apache.isis.applib.AbstractSubscriber;
 import org.apache.isis.applib.annotation.DomainService;
 import org.apache.isis.applib.annotation.NatureOfService;
+import org.axonframework.eventhandling.annotation.EventHandler;
 import org.incode.eurocommercial.relatio.dom.aspect.AspectCreationService;
 import org.incode.eurocommercial.relatio.dom.event.Event;
 
@@ -12,7 +12,7 @@ import javax.inject.Inject;
 @DomainService(nature = NatureOfService.DOMAIN)
 public class EventCreatedSubscriber extends AbstractSubscriber {
 
-    @Subscribe
+    @EventHandler
     public void on(final Event.EventCreatedEvent domainEvent) {
         final Event source = (Event) domainEvent.getSource();
         aspectCreationService.createAspectsFromEvent(source);
