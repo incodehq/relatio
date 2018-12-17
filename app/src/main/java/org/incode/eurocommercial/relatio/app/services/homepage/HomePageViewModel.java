4b5bd70f0331ff9635b4b805d9b715689db9c0f4
Maré du Preez <maredpz@hotmail.nl>
2018-12-17 13:37:22 +0100

    REL-29: Chart beginning

--- app/src/main/java/org/incode/eurocommercial/relatio/app/services/homepage/HomePageViewModel.java
+++ app/src/main/java/org/incode/eurocommercial/relatio/app/services/homepage/HomePageViewModel.java
@@ -16,18 +16,15 @@
  */
 package org.incode.eurocommercial.relatio.app.services.homepage;
 
-import java.util.List;
-
-import javax.inject.Inject;
-
-import org.apache.isis.applib.annotation.Collection;
-import org.apache.isis.applib.annotation.CollectionLayout;
-import org.apache.isis.applib.annotation.Editing;
-import org.apache.isis.applib.annotation.HomePage;
+import com.googlecode.wickedcharts.highcharts.options.*;
+import com.googlecode.wickedcharts.highcharts.options.series.SimpleSeries;
+import org.apache.isis.applib.annotation.Property;
 import org.apache.isis.applib.annotation.ViewModel;
+import org.incode.eurocommercial.relatio.app.services.homepage.chart.WickedChart;
 
-import org.incode.eurocommercial.relatio.dom.profile.Profile;
-import org.incode.eurocommercial.relatio.dom.profile.ProfileRepository;
+import java.util.Arrays;
+
+import static org.apache.isis.applib.annotation.Where.EVERYWHERE;
 
 @ViewModel
 public class HomePageViewModel {
@@ -36,13 +33,25 @@ public class HomePageViewModel {
         return "All Profiles";
     }
 
-    @Collection(editing = Editing.DISABLED)
-    @CollectionLayout(paged=200)
-    @HomePage
-    public List<Profile> getAllProfiles() {
-        return profileRepository.listAll();
+
+    @Property(hidden=EVERYWHERE)
+    public WickedChart getChart() {
+        return setChart();
+    }
+    public WickedChart setChart() {
+        Options options = new Options();
+        options.setChartOptions(new ChartOptions().setType(SeriesType.LINE));
+        options.setTitle(new Title("My very own chart."));
+        options.setxAxis(new Axis().setCategories(Arrays.asList(new String[] { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" })));
+        options.setyAxis(new Axis().setTitle(new Title("Temperature (C)")));
+        options.setLegend(new Legend().setLayout(LegendLayout.VERTICAL).setAlign(HorizontalAlignment.RIGHT).setVerticalAlign(VerticalAlignment.TOP).setX(-10).setY(100).setBorderWidth(0));
+        options.addSeries(new SimpleSeries().setName("Tokyo").setData(Arrays.asList(new Number[] { 7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6 })));
+        options.addSeries(new SimpleSeries().setName("New York").setData(Arrays.asList(new Number[] { -0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5 })));
+        return new WickedChart(options);
     }
 
-    @Inject
-    ProfileRepository profileRepository;
+    public WickedChart showChart() {
+        return getChart();
+    }
+
 }
---
+++ app/src/main/java/org/incode/eurocommercial/relatio/app/services/homepage/chart/WickedChartSemanticsProvider.java
@@ -0,0 +1,103 @@
+package org.incode.eurocommercial.relatio.app.services.homepage.chart;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.isis.applib.adapters.DefaultsProvider;
+import org.apache.isis.applib.adapters.EncoderDecoder;
+import org.apache.isis.applib.adapters.Parser;
+import org.apache.isis.applib.adapters.ValueSemanticsProvider;
+
+import java.io.*;
+
+/**
+ * For internal use; allows Isis to parse etc.
+ */
+public class WickedChartSemanticsProvider implements ValueSemanticsProvider<WickedChart> {
+
+    public boolean isEqualByContent() {
+        return true;
+    }
+
+    public boolean isImmutable() {
+        return true;
+    }
+
+    public Parser<WickedChart> getParser() {
+        return null;
+    }
+
+    public EncoderDecoder<WickedChart> getEncoderDecoder() {
+        // TODO: reconstitute
+        return new EncoderDecoder<WickedChart>() {
+
+            public String toEncodedString(WickedChart toEncode) {
+                return Base64Serializer.toString(toEncode);
+            }
+
+            public WickedChart fromEncodedString(String encodedString) {
+                return (WickedChart) Base64Serializer.fromString(encodedString);
+            }
+        };
+    }
+
+    public DefaultsProvider<WickedChart> getDefaultsProvider() {
+        return null;
+    }
+
+}
+
+/**
+ *
+ *copied from org.apache.isis.core:isis-core-metamodel
+ */
+class Base64Serializer {
+
+    public static class Exception extends RuntimeException {
+
+        private static final long serialVersionUID = 1L;
+
+        public Exception(java.lang.Exception e) {
+            super(e);
+        }
+    }
+
+    static Object fromString( String s ) {
+        final byte [] data = Base64.decodeBase64( s );
+        ObjectInputStream ois = null;
+        try {
+            ois = new ObjectInputStream(new ByteArrayInputStream(  data ) );
+            return ois.readObject();
+        } catch (IOException e) {
+            throw new Base64Serializer.Exception(e);
+        } catch (ClassNotFoundException e) {
+            throw new Base64Serializer.Exception(e);
+        } finally {
+            try {
+                if(ois != null) {
+                    ois.close();
+                }
+            } catch (IOException e) {
+                throw new Base64Serializer.Exception(e);
+            }
+        }
+    }
+
+    static String toString( Serializable serializable ) {
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = null;
+        try {
+            oos = new ObjectOutputStream( baos );
+            oos.writeObject( serializable );
+            return new String( Base64.encodeBase64( baos.toByteArray() ) );
+        } catch (IOException e) {
+            throw new Base64Serializer.Exception(e);
+        } finally {
+            try {
+                if(oos != null) {
+                    oos.close();
+                }
+            } catch (IOException e) {
+                throw new Base64Serializer.Exception(e);
+            }
+        }
+    }
+}
