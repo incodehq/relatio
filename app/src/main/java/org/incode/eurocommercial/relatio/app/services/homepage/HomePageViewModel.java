367aada5a5cc657c6a60b01861d8c4eceea10297
Marcus van Bergen <mbhjvanbergen@gmail.com>
2018-12-19 14:36:58 +0100

    REL-29: updating to pieChart

--- app/src/main/java/org/incode/eurocommercial/relatio/app/services/homepage/HomePageViewModel.java
+++ app/src/main/java/org/incode/eurocommercial/relatio/app/services/homepage/HomePageViewModel.java
@@ -16,33 +16,29 @@
  */
 package org.incode.eurocommercial.relatio.app.services.homepage;
 
-import java.util.Arrays;
-
 import com.googlecode.wickedcharts.highcharts.options.Axis;
 import com.googlecode.wickedcharts.highcharts.options.ChartOptions;
-import com.googlecode.wickedcharts.highcharts.options.HorizontalAlignment;
-import com.googlecode.wickedcharts.highcharts.options.Legend;
-import com.googlecode.wickedcharts.highcharts.options.LegendLayout;
 import com.googlecode.wickedcharts.highcharts.options.Options;
 import com.googlecode.wickedcharts.highcharts.options.SeriesType;
 import com.googlecode.wickedcharts.highcharts.options.Title;
-import com.googlecode.wickedcharts.highcharts.options.VerticalAlignment;
 import com.googlecode.wickedcharts.highcharts.options.series.SimpleSeries;
-
 import org.apache.isis.applib.annotation.Property;
 import org.apache.isis.applib.annotation.ViewModel;
-
+import static org.apache.isis.applib.annotation.Where.EVERYWHERE;
+import org.incode.eurocommercial.relatio.dom.profile.Profile;
+import org.incode.eurocommercial.relatio.dom.profile.ProfileRepository;
 import org.isisaddons.wicket.wickedcharts.cpt.applib.WickedChart;
 
-import static org.apache.isis.applib.annotation.Where.EVERYWHERE;
+import javax.inject.Inject;
+import java.util.Arrays;
+import java.util.List;
 
 @ViewModel
 public class HomePageViewModel {
 
     public String title() {
-        return "All Profiles";
+        return "Home Page";
     }
-
 
     @Property(hidden=EVERYWHERE)
     public WickedChart getChart() {
@@ -50,18 +46,27 @@ public class HomePageViewModel {
     }
     public WickedChart setChart() {
         Options options = new Options();
-        options.setChartOptions(new ChartOptions().setType(SeriesType.LINE));
-        options.setTitle(new Title("My very own chart."));
-        options.setxAxis(new Axis().setCategories(Arrays.asList(new String[] { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" })));
-        options.setyAxis(new Axis().setTitle(new Title("Temperature (C)")));
-        options.setLegend(new Legend().setLayout(LegendLayout.VERTICAL).setAlign(HorizontalAlignment.RIGHT).setVerticalAlign(VerticalAlignment.TOP).setX(-10).setY(100).setBorderWidth(0));
-        options.addSeries(new SimpleSeries().setName("Tokyo").setData(Arrays.asList(new Number[] { 7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6 })));
-        options.addSeries(new SimpleSeries().setName("New York").setData(Arrays.asList(new Number[] { -0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5 })));
+        options.setChartOptions(new ChartOptions().setType(SeriesType.PIE).);
+        options.setTitle(new Title("Marketing consent True/False"));
+
+        options.setxAxis(new Axis().setCategories(Arrays.asList(new String[] { "True", "False"})));
+
+
+        List<Profile> profiles = profileRepository.listAll();
+
+        Number numberOfProfilesWithMarketingConsentTrue = profiles.stream().filter(profile -> profile.getMarketingConsent() != null && profile.getMarketingConsent().equals(Boolean.TRUE)).count();
+        Number numberOfProfilesWithMarketingConsentFalse = profiles.stream().filter(profile -> profile.getMarketingConsent() != null && profile.getMarketingConsent().equals(Boolean.FALSE)).count();
+
+        options.addSeries(new SimpleSeries().setName("Data").setData(Arrays.asList(new Number[] { numberOfProfilesWithMarketingConsentTrue, numberOfProfilesWithMarketingConsentFalse })));
+
         return new WickedChart(options);
     }
 
     public WickedChart showChart() {
         return getChart();
     }
+
+    @Inject
+    ProfileRepository profileRepository;
 
 }
